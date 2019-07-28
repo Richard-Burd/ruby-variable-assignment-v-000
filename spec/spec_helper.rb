e0428be208fb3b8e6a21426925aa167fc446c7fa
Richard Burd <richardburdx@yahoo.com>
2019-07-28 00:27:08 +0000

    Automatically backed up by Learn

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,18 +14,3 @@ def get_variable_from_file(file, variable)
     raise NameError, "local variable `#{variable}` not defined in #{file}."
   end
 end
-
-def burd_made_this_to_reverse_engineer_rspec_tests(some_file, some_variable, another_variable)
-  file_scope = binding
-  # burd doesn't know what binding does yet
-
-  file_scope.eval(File.read(some_file))
-  # burd thinks this reads the files like "burdtest.rb" and "variable.rb"
-
-  begin
-    return file_scope.local_variable_get(some_variable)
-  rescue NameError
-  # this must stop everything when the test fails
-    raise NameError, "A mysterious variable `#{some_variable}` created by Burd is not defined in the #{some_file} file."
-  end
-end
